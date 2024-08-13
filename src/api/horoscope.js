d1479247f49a6db23e46ecd058360961d9fa6978
Ivan Bezniak <ivan.bezniak@gmail.com>
2024-08-13 20:45:45 +0300

    3 commit

--- src/api/horoscope.js
+++ src/api/horoscope.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 export const getHoroscope = async (sign, language) => {
-    const response = await axios.post(process.env.REACT_APP_API_URL, {
+    const response = await axios.post('http://5.35.90.171:61011/get_horoscope', {
         language
     });
     return response.data;
