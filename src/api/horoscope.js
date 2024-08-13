5b9d776465b58f2634e9745a547101a9787b58ac
Ivan Bezniak <ivan.bezniak@gmail.com>
2024-08-13 20:52:22 +0300

    3 commit

--- src/api/horoscope.js
+++ src/api/horoscope.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 
 
 export const getHoroscope = async (sign, language) => {
-    const response = await axios.post('http://5.35.90.171:61011/get_horoscope', {
+    const response = await axios.post('https://poker247tech.ru/get_horoscope/', {
         language
     });
+    console.log(response.data)
     return response.data;
 };
